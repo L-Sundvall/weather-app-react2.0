9dd957e5de144e022e793f1f2a696b14335fea93
Linda Sundvall <73383816+L-Sundvall@users.noreply.github.com>
2021-01-11 15:58:39 +0100

    added Search input and position button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Search from './Search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+     <Search />
        
       </header>
     </div>
--- src/Search.js
+++ src/Search.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+export default function Search(){
+return (
+<div>
+            <form>
+              <input
+                type="search"
+                placeholder="Search for a city"/>
+              <input type="submit" value="Search" className="Search-button" />
+            </form>
+            <button className="current-button">Current position</button>
+          </div>); }
